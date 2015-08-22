2e0aa419dbdc42da29f199fc974ae13855a7c538
vajragrrrl <nicole@thinkingsage.com>
2015-08-22 18:04:07 -0400

    Update run_Analysis.R

--- run_Analysis.R
+++ run_Analysis.R
@@ -1,11 +1,9 @@
-
-## Goals
-## 1. each variable should be in one column
-## 2. each observation of that variable should be in a diferent row
-## 3. include ids to link tables together
-
-## Merges the training and the test sets to create one data set.
-
+## Instructions
+## 1) Merges the training and the test sets to create one data set.
+## 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3) Uses descriptive activity names to name the activities in the data set
+## 4) Appropriately labels the data set with descriptive variable names. 
+## 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 ## Load packages
 library(plyr)
@@ -35,7 +33,6 @@ dsetin$test <- cbind(subject=read.table(paste(wd, "test", "subject_test.txt", se
 ## Visual sanity check - get familiar with data
 ##   - expecting to see Activities 1-6 populated for each subject
 table(dsetin$test$Subject,dsetin$test$Activity.ID)
-
 
 ## Read in train data
 dsetin$train <- cbind(subject=read.table(paste(wd, "train", "subject_train.txt", sep="/"), col.names="Subject"),
